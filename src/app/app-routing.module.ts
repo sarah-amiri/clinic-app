68318c3dd3c20d52a212337cfc28c69a5130c748
saraha70 <40001248+saraha70@users.noreply.github.com>
2019-10-09 11:14:02 +0330

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+﻿import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DoctorsComponent } from './doctors/doctors.component';
+import { DoctorDetailComponent } from './doctors/doctor-detail/doctor-detail.component';
+import { ClinicsComponent } from './clinics/clinics.component';
+import { ClinicDetailComponent } from './clinics/clinic-detail/clinic-detail.component';
+
+const routes: Routes = [
+    { path: '', component: DashboardComponent, pathMatch: 'full' },
+    {
+        path: 'doctors', children: [
+            { path: '', component: DoctorsComponent, pathMatch: 'full' },
+            { path: ':id', component: DoctorDetailComponent }
+        ]
+    },
+    {
+        path: 'clinics', component: ClinicsComponent, children: [
+            { path: ':name', component: ClinicDetailComponent }
+        ]
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+﻿import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { DoctorsComponent } from './doctors/doctors.component';
+import { DoctorListComponent } from './doctors/doctor-list/doctor-list.component';
+import { DoctorDetailComponent } from './doctors/doctor-detail/doctor-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ClinicsComponent } from './clinics/clinics.component';
+import { ClinicsListComponent } from './clinics/clinics-list/clinics-list.component';
+import { ClinicDetailComponent } from './clinics/clinic-detail/clinic-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DoctorsComponent,
+    DoctorListComponent ,
+    DoctorDetailComponent ,
+    DashboardComponent,
+    ClinicDetailComponent,
+    ClinicsListComponent,
+      ClinicsComponent,
+    ],
+  imports: [
+      BrowserModule,
+      AppRoutingModule,
+      HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
